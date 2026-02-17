c37ab2a6dcfd5dc91693827ac8747ed16a2e9808
Aadarsh Kumar <165693609+AADARSHKUMAR45@users.noreply.github.com>
2026-02-17 22:27:03 +0530

    Add files via upload

--- bubbleSort.java
+++ bubbleSort.java
@@ -1,15 +1,22 @@
 public class bubbleSort {
     static void BubbleSort(int[] arr){
-        for(int turn=0;turn<arr.length-1;turn++){
-            for(int i=0;i<arr.length-turn-1;i++){
-                if(arr[i]>arr[i+1]){
-                    int temp=arr[i];
-                    arr[i]=arr[i+1];
-                    arr[i+1]=temp;
+            for (int turn = 0; turn < arr.length - 1; turn++) {
+                boolean swapped = false;
+
+                for (int i = 0; i < arr.length - turn - 1; i++) {
+                    if (arr[i] > arr[i + 1]) {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+                if (!swapped) {
+                    break;
+
                 }
             }
         }
-    }
     static void print(int arr[]){
         for(int i=0;i<arr.length;i++){
             System.out.print(arr[i]+" ");
@@ -17,7 +24,7 @@ public class bubbleSort {
         System.out.println();
     }
     public static void main(String[] args) {
-        int arr[]={5,4,2,3,1};
+        int arr[]={1,2,7,4,5};
         BubbleSort(arr);
         print(arr);
     }
