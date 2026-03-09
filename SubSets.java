9bdb7b5c4844e2ceb95fe5d6c783b05531f2490c
Aadarsh Kumar <165693609+AADARSHKUMAR45@users.noreply.github.com>
2026-03-09 23:25:42 +0530

    Add files via upload

---
+++ SubSets.java
@@ -0,0 +1,25 @@
+public class SubSets {
+    public static void subsetprint(String str,String ans ,int i){
+        if(i==str.length()){
+            if(ans.length()==0){
+                System.out.println("null");
+            }
+            else{
+                System.out.println(ans);
+            }
+            return;
+
+        }
+
+        //yes choice
+        subsetprint(str, ans+str.charAt(i),i+1);
+        //no choice
+        subsetprint(str, ans, i+1);
+
+    }
+    public static void main(String[] args) {
+        String str="abc";
+        subsetprint(str, "", 0);
+    }
+    
+}
