c9a58f63cbe21d75e249efa8bd4ab900bd95cfbc
Aadarsh Kumar <165693609+AADARSHKUMAR45@users.noreply.github.com>
2026-03-22 22:43:16 +0530

    Add files via upload

---
+++ LinkedListCycle.java
@@ -0,0 +1,44 @@
+public class LinkedListCycle {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+            this.next = null;
+        }
+    }
+
+    // Function to detect cycle
+    public static boolean hasCycle(ListNode head) {
+        if (head == null) return false;
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;          
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true; 
+            }
+        }
+        return false;     
+    }
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+        head.next.next.next.next.next = head.next.next;
+        boolean result = hasCycle(head);
+
+        if (result) {
+            System.out.println("Cycle detected");
+        } else {
+            System.out.println("No cycle");
+        }
+    }
+}
