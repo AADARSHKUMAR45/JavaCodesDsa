2531c96d67ecbb63e4a524ba03f0691355d5d04f
Aadarsh Kumar <165693609+AADARSHKUMAR45@users.noreply.github.com>
2026-03-23 23:16:07 +0530

    Add files via upload

---
+++ SortList.java
@@ -0,0 +1,76 @@
+public class SortList {
+    static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int val) {
+            this.val = val;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Create list: 4 -> 2 -> 1 -> 3
+        ListNode head = new ListNode(4);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(1);
+        head.next.next.next = new ListNode(3);
+
+        System.out.print("Before Sorting: ");
+        print(head);
+
+        head = sortList(head);
+
+        System.out.print("After Sorting: ");
+        print(head);
+    }
+    public static ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) return head;
+
+        ListNode mid = getMid(head);
+        ListNode right = mid.next;
+        mid.next = null;
+
+        ListNode leftSorted = sortList(head);
+        ListNode rightSorted = sortList(right);
+
+        return merge(leftSorted, rightSorted);
+    }
+    public static ListNode getMid(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+    public static ListNode merge(ListNode l1, ListNode l2) {
+        ListNode dummy = new ListNode(-1);
+        ListNode temp = dummy;
+
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2.val) {
+                temp.next = l1;
+                l1 = l1.next;
+            } else {
+                temp.next = l2;
+                l2 = l2.next;
+            }
+            temp = temp.next;
+        }
+
+        if (l1 != null) temp.next = l1;
+        if (l2 != null) temp.next = l2;
+
+        return dummy.next;
+    }
+    public static void print(ListNode head) {
+        while (head != null) {
+            System.out.print(head.val + " -> ");
+            head = head.next;
+        }
+        System.out.println("null");
+    }
+}
